3e2b6fc5cf4f8e12539a3e8db684c3a7d47bee97
Prasum <sumpra907@gmail.com>
2024-11-22 12:38:10 +0545

    Initial commit

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import './App.css'
+import FruitList from "./components/Fruits.jsx";
+
+const App = () => {
+    return(
+        <div className="App">
+            <header className="App-header">
+                <h1>Fruit Management App</h1>
+            </header>
+            <main>
+            <FruitList/>
+            </main>
+        </div>
+    )
+}
+export default App
